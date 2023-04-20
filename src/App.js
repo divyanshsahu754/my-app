273aedf692c04aa3aa9fc821091230545583c890
divyanshsahu754 <divyanshsahu754@gmail.com>
2023-04-20 18:42:16 +0530

    initia

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+        <h2>
+          Benifits of using React
+        </h2>
+        <p>
+         1. Component Based-Architecture
+        </p>
+        <p>
+          2. Virtual DOM for efficient updates
+        </p>
+        <p>
+          3. Rich Ecosystem and community
+        </p>
+        <p>
+          4. Cross-platform Development
+        </p>
+        <p>
+          5. Strong commmunity support;
+        </p>
+</div>
+        <button>
+          Get-Started.....
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
